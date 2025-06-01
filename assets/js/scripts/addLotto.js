c01151d2b27cd40801fe160c7a0fff45a0b48c24
super-nikorn <nikorn.pond@gmail.com>
2025-06-01 13:04:32 +0700

    010668 1304

---
+++ assets/js/scripts/addLotto.js
@@ -0,0 +1,16 @@
+import { db } from '../api/firebaseConfig.js';
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+export async function saveToDatabase(payload) {
+  const buyer = payload["ชื่อผู้ซื้อ"];
+  const nowDate = new Date().toISOString().split("T")[0];
+  const documentId = `${nowDate}_${buyer.replace(/\s+/g, "_")}`;
+
+  try {
+    await setDoc(doc(db, "lottoTickets", documentId), payload);
+    alert("✅ บันทึกโพยลง Firestore เรียบร้อยแล้ว");
+  } catch (error) {
+    console.error("❌ บันทึกไม่สำเร็จ:", error);
+    alert("❌ เกิดข้อผิดพลาดในการบันทึกข้อมูล");
+  }
+}
